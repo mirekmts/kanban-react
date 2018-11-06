a65823d7eca8392bde9c802264c12b65ace7ede6
mirekmts <mirekmts@gmail.com>
2018-11-06 16:50:00 +0100

    Setup project: add redux, setup tests and add library

--- src/index.js
+++ src/index.js
@@ -3,10 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import  { createStore, applyMiddleware } from 'redux'
+import rootReducer from './reducers'
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const middlewares = [thunk];
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+if (process.env.NODE_ENV !== 'production') {
+  const { whyDidYouUpdate } = require('why-did-you-update');
+  const { logger } = require(`redux-logger`);
+
+  middlewares.push(logger);
+  whyDidYouUpdate(React);
+}
+
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(
+      applyMiddleware(...middlewares),
+    )
+)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
+
 serviceWorker.unregister();
