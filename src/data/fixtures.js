c6eed68b8096caa81dacf991d85ed0b9307bb374
mirekmts <mirekmts@gmail.com>
2018-11-06 17:51:52 +0100

    Add fixtures and test Task

---
+++ src/data/fixtures.js
@@ -0,0 +1,29 @@
+export const list = {
+  id: 1,
+  name: 'Backlog',
+  items: [
+      {
+          id: 1,
+          name: 'Power up the robot',
+          description: 'description',
+      },
+      {
+          id: 2,
+          name: 'Submit chatbot build for review',
+          description: 'description',
+      },
+  ],
+}
+
+export const item = {
+    id: 3,
+    name: 'Chatbot',
+    description: 'Submit chatbot build for review',
+}
+
+export const newItem = {
+    name: 'Chatbot',
+    description: 'Submit chatbot build for review',
+}
+
+export const newListName = 'Done'
